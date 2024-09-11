fc646aa8565120561981af0d59f935a9b282a7db
naonaosun <pepebobby@icloud.com>
2024-09-11 13:52:21 +0900

    detaフィールドの削除

--- config.py
+++ config.py
@@ -4,6 +4,7 @@ from peewee import (
     SqliteDatabase,
     Model,
     IntegerField,
+    IntegrityError,
     CharField,
     TextField,
     TimestampField,
@@ -17,15 +18,16 @@ from playhouse.migrate import SqliteMigrator, migrate
 db = SqliteDatabase("db.sqlite")
 db.connect()  # データベースに接続
 
-# マイグレーターの設定
-migrator = SqliteMigrator(db)
+# # マイグレーターの設定
+# migrator = SqliteMigrator(db)
 
-# トランザクションを開始してマイグレーションを実行
-with db.atomic():
-    migrate(
-        migrator.add_column('events', 'start_date', DateTimeField(null=True)),
-        migrator.add_column('events', 'end_date', DateTimeField(null=True))
-    )
+# # トランザクションを開始してマイグレーションを実行
+# with db.atomic():
+#     migrate(
+#         migrator.add_column('events', 'start_date', DateTimeField(null=True)),
+#         migrator.add_column('events', 'end_date', DateTimeField(null=True))
+#     )
+
 
 class User(UserMixin, Model):
     id = IntegerField(primary_key=True)
@@ -41,8 +43,8 @@ class User(UserMixin, Model):
 class Event(Model):
     name = CharField()
     content = TextField(null=True)
-    start_date = DateTimeField()
-    end_date = DateTimeField()
+    start_date = DateTimeField(null=True)
+    end_date = DateTimeField(null=True)
     place = CharField()
     address = CharField()
     url = CharField(null=True)
@@ -51,6 +53,41 @@ class Event(Model):
     class Meta:
         database = db
         table_name = "events"
+
+#★detaフィールドの削除★1～5を順を追って実行
+## 1. 一時テーブルを作成し、データをコピー
+# db.execute_sql('''
+#     CREATE TABLE IF NOT EXISTS events_backup AS SELECT 
+#     name, content, start_date, end_date, place, address, url, image 
+#     FROM events;
+# ''')
+
+## 2. 元のテーブルを削除
+# db.execute_sql('DROP TABLE events;')
+
+## 3. 新しいスキーマでテーブルを作成
+# db.create_tables([Event])
+
+## 4. データを一時テーブルから新しいテーブルにコピー
+# db.execute_sql('''
+#     INSERT INTO events (name, content, start_date, end_date, place, address, url, image)
+#     SELECT name, content, start_date, end_date, place, address, url, image FROM events_backup;
+# ''')
+
+# # 5. 一時テーブルを削除
+# db.execute_sql('DROP TABLE events_backup;')
+
+
+
+
+
+
+# def save(self, *args, **kwargs):
+#     # バリデーションチェック
+#     if self.start_date > self.end_date:
+#         raise ValueError("Start date must be before end date.")
+
+#     return super().save(*args, **kwargs)
 
 
 # class Image(Model):
