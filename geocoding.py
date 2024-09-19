1278b65ed8ea8eb69b053fdc10b8522a3299b77e
naonaosun <pepebobby@icloud.com>
2024-09-19 14:29:45 +0900

    緯度・経度を抽出/補足説明追加

--- geocoding.py
+++ geocoding.py
@@ -5,7 +5,8 @@ address = "岩手県盛岡市本宮荒屋25-11"
 
 response = requests.get(f"https://www.geocoding.jp/api/?q={address}")
 
-# XMLを辞書型に変換
+# ★このAPIからAPIから返されるデータはXML形式なため、response.textにしているが、これをそのまま使うとただの文字列のため要素を簡単に取り出せない▶辞書型に変更する必要があり、するとrezultという要素もできる！
+# XMLを辞書型に変換///
 data_dict = xmltodict.parse(response.text)
 
 # 辞書から緯度と経度を取得
@@ -14,4 +15,6 @@ lng = data_dict['result']['coordinate']['lng']
 
 print(f"Latitude: {lat}")
 print(f"Longitude: {lng}")
+
+
 
