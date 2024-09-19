2627fbfdbd25affe9048cbbe63070307b0c1dcea
naonaosun <pepebobby@icloud.com>
2024-09-19 22:36:24 +0900

    緯度経度の抽出を関数化

--- geocoding.py
+++ geocoding.py
@@ -1,20 +1,31 @@
 import requests
 import xmltodict
 
-address = "岩手県盛岡市本宮荒屋25-11"
+# address = "岩手県盛岡市本宮荒屋25-11"
 
-response = requests.get(f"https://www.geocoding.jp/api/?q={address}")
+# response = requests.get(f"https://www.geocoding.jp/api/?q={address}")
 
-# ★このAPIからAPIから返されるデータはXML形式なため、response.textにしているが、これをそのまま使うとただの文字列のため要素を簡単に取り出せない▶辞書型に変更する必要があり、するとrezultという要素もできる！
-# XMLを辞書型に変換///
-data_dict = xmltodict.parse(response.text)
+# # ★このAPIからAPIから返されるデータはXML形式なため、response.textにしているが、これをそのまま使うとただの文字列のため要素を簡単に取り出せない▶辞書型に変更する必要があり、するとrezultという要素もできる！
+# # XMLを辞書型に変換///
+# data_dict = xmltodict.parse(response.text)
 
-# 辞書から緯度と経度を取得
-lat = data_dict['result']['coordinate']['lat']
-lng = data_dict['result']['coordinate']['lng']
+# # 辞書から緯度と経度を取得
+# lat = data_dict['result']['coordinate']['lat']
+# lng = data_dict['result']['coordinate']['lng']
 
-print(f"Latitude: {lat}")
-print(f"Longitude: {lng}")
+# print(f"Latitude: {lat}")
+# print(f"Longitude: {lng}")
 
 
+def get_lat_lng(address):
+    response = requests.get(f"https://www.geocoding.jp/api/?q={address}")
+
+    # XMLを辞書型に変換
+    data_dict = xmltodict.parse(response.text)
+
+    # 辞書から緯度と経度を取得
+    lat = data_dict["result"]["coordinate"]["lat"]
+    lng = data_dict["result"]["coordinate"]["lng"]
+
+    return lat, lng
 
